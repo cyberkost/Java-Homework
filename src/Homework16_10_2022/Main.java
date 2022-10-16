b0acbcbaccab4fb7c333544efc49b1d4a1098966
Konstantin Bezgatsev <gelandewagen1@gmail.com>
2022-10-16 17:27:12 +0200

    homework

--- src/Homework16_10_2022/Main.java
+++ src/Homework16_10_2022/Main.java
@@ -6,23 +6,13 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         String[] array = new String[22];
-        int[] array1 = {5, 10, 3, 2, 1, 9,};
+        int[] array1 = {5, 10, 3, 2, 1, 9};
         char[] array2 = new char[5];
         testFori(array);
         testForeach(array1);
         testForiIf(array1);
         testWhile(array1);
         testChar(array2);
-    }
-    private static void testWhile(int[] array1){
-        int sum = 0; // суммарное количество оценок
-        int i = 0;
-        while (i < array1.length) {
-            int grade = array1[i];
-            sum += grade;
-            i++;
-        }
-        System.out.println("[While] Среднея оценка по массиву = " + (sum / array1.length));
     }
     private static void testFori(String[] array) {
         for (int i = 0; i < array.length; i++) {
@@ -46,13 +36,22 @@ public class Main {
         }
         System.out.println("[ForiIf] Лучшая (максимальная) оценка массива = " + maxGrade);
     }
+    private static void testWhile(int[] array1){
+        int sum = 0; // суммарное количество оценок
+        int i = 0;
+        while (i < array1.length) {
+            int grade = array1[i];
+            sum += grade;
+            i++;
+        }
+        System.out.println("[While] Среднея оценка по массиву = " + (sum / array1.length));
+    }
     private static void testChar(char[] array2) {
         for (char i = 0; i < array2.length; i++) {
             array2[i] = (char) new Random().nextInt(10);
         }
         System.out.println("[Char] Рандомные символы из массива char = " + Arrays.toString(array2));
     }
-
 }
 
 
